c5d0ae7c342185b45ab9f6038a3dccb7e44c9387
Haoyang Li <lancersaber575@gmail.com>
2023-11-26 11:23:52 -0800

    Updated enter model and description for Posting.js, by Haoyang

---
+++ website/src/pages/Posting.js
@@ -0,0 +1,74 @@
+import { Link } from 'react-router-dom';
+import React, {useState} from 'react'
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { hamburgerMenu } from './hamburgerMenu';
+import { IconContext } from 'react-icons';
+import './Signup.css'
+
+function Posting() {
+  const [sidebar, setSidebar] = useState(false) 
+  /*setSidebar=update*/ /*false means the current value is not showing*/
+  const showSidebar = () =>  setSidebar(!sidebar)
+  /* utilize set side bar and this is going to update the value to whatever the opposite of it is currently*/
+  /*(!sidebar) it's reversing the value true/false*/
+
+  return (
+      <>
+      <IconContext.Provider value= {{color: '#fff'}}>
+          <div className='navbar'>
+              <Link to="#" className='hamburger-bars'>
+                  <FaBars onClick={showSidebar} />
+              </Link>
+
+              <div className="logo">
+                  <img src="https://www.clker.com/cliparts/u/O/L/Q/c/m/car-icon-hi.png" alt="Logo" />
+              </div>
+          </div>
+      
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+              <li className="navbar-toggle">
+                  <Link to="#" className='hamburger-bars'>
+                      <AiOutlineClose />
+                  </Link>
+              </li>
+              {hamburgerMenu.map((item, index) => {
+                  return (
+                      <li key={index} className={item.cName}>
+                          <Link to={item.path}>
+                              {item.icon}
+                              <span>{item.title}</span>
+                          </Link>
+                      </li>
+                  );
+              })}
+          </ul>
+      </nav>
+      </IconContext.Provider>
+
+      <div className='posting'>
+          <h1>posting page</h1>
+          <div className = "Enter Information">
+          <h2>Enter the Information Required</h2>
+            <form>
+                <label>Enter the model: </label>
+                <textarea
+                    required
+                ></textarea>
+            </form>
+            <form>
+                <label>Enter the description: </label>
+                <textarea
+                    required
+                ></textarea>
+            </form>
+          </div>
+
+        </div>
+
+      </>
+  )
+}
+
+export default Posting
