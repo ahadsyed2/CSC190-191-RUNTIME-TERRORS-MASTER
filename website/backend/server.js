29708dd5aaff2f9b8b04c2813048bae51fceb49b
NC-CSUS <113408730+NC-CSUS@users.noreply.github.com>
2024-02-11 14:24:22 -0800

    Update server.js
    Whitespace causing merge conflicts, no change to the code

--- website/backend/server.js
+++ website/backend/server.js
@@ -1,57 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
-const userRoutes = require ('./routes/userRoutes')
-const postingRoutes = require('./routes/postingRoutes')
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
 
+const userRoutes = require('./routes/userRoutes');
+const postingRoutes = require('./routes/postingRoutes');
 
-//express app
-const app = express()
+const app = express();
 
-//middleware, parse json body for post request
-app.use(express.json())
-
-
+// Middleware to log path and request type
 app.use((req, res, next) => {
-    //LOG PATH AND REQUEST TYPE
-    console.log(req.path, req.method)
-    next()
-})
+  console.log(req.path, req.method);
+  next();
+});
+
+// Middleware to parse JSON body for POST requests
+app.use(express.json());
+
+// Routes
+app.use('/api/userRoutes', userRoutes);
+app.use('/api/postingRoutes', postingRoutes);
+app.use('/api/profile', profileRoutes);
+app.use('/api/postRoutes', postRoutes); //I used this for home page -Nick
 
 
-//routes, fires router (carmonyRoutes) from carmonyRoutes file
 
-//recieve and execute requests
-app.use('/api/userRoutes', userRoutes)
-
-//recieve and execute requests from posting page
-app.use('/api/postingRoutes', postingRoutes)
-
-//recieve and execute requests from posting page
-app.use('/api/postRoutes', postRoutes)
-
-//recieve and execute requests from profile page
-app.use('/api/profile', profileRoutes)
-
-//app.get('/', (req,res) => {
-
-    //res.json({mssg:"welcome to the website"})
-
-//})
-
-//connect to db
-mongoose.connect(process.env.MONGO_URI)
-    //  once connected, listen for requests
-    .then(() => {
-        app.listen(process.env.PORT, () => {
-            console.log('CONNECTED. listening on port 4k')
-        })
-    })
-
-
-    //if any error example wrong mongo uri
-    .catch((error) =>{
-        console.log(error)
-    }
-    )
-
+// Connect to MongoDB and start the server
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log('CONNECTED. Listening on port 4k');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
