8c4d7aed63505924d14ad46ad7c02d94b1d0226e
Haoyang Li <lancersaber575@gmail.com>
2024-02-25 19:14:19 -0800

    JIRA task #75: user can log in to facebook and user profile after login

--- website/src/pages/Facebooklogin.js
+++ website/src/pages/Facebooklogin.js
@@ -1,38 +1,33 @@
-import { Link } from 'react-router-dom';
-import React from 'react'
+import { useState } from 'react';
 import {LoginSocialFacebook} from 'reactjs-social-login';
-import {FacebookLoginButton} from 'react-social-login-buttons'
-import './Facebooklogin.css';
-import {useState} from 'react'
+import {FacebookLoginButton} from 'react-social-login-buttons';
+//import './Facebooklogin.css';
+
 
 function Facebooklogin() {
-    const [profile,setProfile]=useState(null);
+    const [profile, setProfile] = useState(null);
+
     return (
-    <div className="FacebookLoginContainer">
-    {!profile ? <LoginSocialFacebook
-        appId="256005617100674"
-        onResolve={(response) => {
+        <div>
+        {!profile ? <LoginSocialFacebook
+        appId='667815395370051'
+        onResolve={(response)=>{
             console.log(response);
-            setProfile(response.data)
+            setProfile(response.data);
         }}
-        onReject={(error) => {
+        onReject={(error)=>{
             console.log(error);
         }}
         >
-        <button className="FacebookLoginButton">
-            <img
-            src="https://seeklogo.com/images/F/facebook-icon-logo-819DD0A07B-seeklogo.com.png"
-            alt="Facebook"
-            />
-            Continue with Facebook
-        </button>
-        </LoginSocialFacebook> :''}
-        {profile? <div>
+            <FacebookLoginButton/>
+        </LoginSocialFacebook> : ''}
+        
+
+        {profile ? <div>
             <h1>{profile.name}</h1>
-            <img src = {profile.picture.data.url}/>
-            
-    </div>:''}
-    </div>
+            <img src = {profile.picture.data.url} />
+        </div> : ''}
+        </div>
     );
 }
 
