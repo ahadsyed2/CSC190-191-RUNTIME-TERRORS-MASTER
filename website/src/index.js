bbbb317c9b03aabbb0a432d974fa039e1f60d8d1
janeeya chanta <janeeyachanta@janeeyas-MacBook-Pro.local>
2024-03-10 20:07:11 -0700

    tailwind installation files

--- website/src/index.js
+++ website/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css'
 import { AuthContextProvider } from './context/AuthContext';
 import { ProfileContextProvider } from './context/ProfileContext';
 import { PostContextProvider } from './context/PostContext';
